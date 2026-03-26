566a739c137c158ced1778ae843423758562fd6b
Victor Prieto <victorpre04@gmail.com>
2026-03-25 19:57:28 -0500

    agregado README y corrección de script

---
+++ README.md
@@ -0,0 +1,5 @@
+# Mis Proyectos de ADSO
+Este repositorio contiene mis avances en desarrollo de software y ciberseguridad.
+
+## Scripts Incluidos:
+- **mi_primera_red.py**: Un monitor de red básico en Python que verifica conectividad mediante PING.
--- mi_primera_red.py
+++ mi_primera_red.py
@@ -1,1 +1,13 @@
-print("Primer paso en ADSO y Ciberseguridad con Python")
+# Mi primer script de seguridad en Python
+import os
+
+print("--- BIENVENIDO AL MONITOR DE RED DE VICTOR ---")
+ip = input("Introduce una dirección IP para probar conexion (ejemplo: 8.8.8.8): ")
+
+print(f"Verificando estado de {ip}...")
+respuesta = os.system(f"ping -c 4 {ip}")
+
+if respuesta == 0:
+   print(f"\n[ÉXITO] La IP {ip} esta activa.")
+else:
+   print(f"\n[ALERTA] no se pudo contactar con {ip}.")
